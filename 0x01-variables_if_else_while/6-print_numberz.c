29eaf463b8f78a94c76c87c930677e155cd043a0
Doziechucks <chiedoziechukwukam@gmail.com>
2023-08-12 18:06:15 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - printing numbers in base ten
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+		putchar(x);
+	putchar('\n');
+	return (0);
+}
