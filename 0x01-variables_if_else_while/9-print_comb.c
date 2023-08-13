d4b006fcc1887b0ad2b6f08111d9c6fca54b733d
Doziechucks <chiedoziechukwukam@gmail.com>
2023-08-12 19:23:14 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - printing number combos
+ * Return: 0(successful)
+ */
+int main(void)
+{
+	int x;
+
+	for (a = 0; x < 10; x++)
+	{
+		putchar(x + '0');
+		if (x < 9)
+		{
+			putchar(',');
+			putchar(' ';)
+		}
+	putchar('\n');
+		return (0)
+	}
+}
