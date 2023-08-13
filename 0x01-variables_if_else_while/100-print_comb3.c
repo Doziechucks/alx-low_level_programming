fa3b7588d91787c226e5f4c5f512943f19a08f94
Doziechucks <chiedoziechukwukam@gmail.com>
2023-08-13 16:00:54 -0700

    printing double digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,17 +5,17 @@
  */
 int main(void)
 {
-	int x = '0';
-	int y = '0';
+	int x = 0;
+	int y = 0;
 
-	for (y = '0'; y <= '9'; y++)
+	for (y = 0; y <= 9; y++)
 	{
-		for (x = 0; x <= '9'; x++)
+		for (x = 0; x <= 9; x++)
 		{
 			if (!((x == y)) || ((y > x)))
 			{
-				putchar(y);
-				putchar(x);
+				putchar('0' + y);
+				putchar('0' + x);
 				if (!(x == '9' && y == '8'))
 				{
 					putchar(',');
