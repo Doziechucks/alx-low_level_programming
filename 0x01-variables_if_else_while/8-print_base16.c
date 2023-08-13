1281e99a2faeb8f265869a754721b6ad6bc4c3cd
Doziechucks <chiedoziechukwukam@gmail.com>
2023-08-12 18:52:26 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - printing 16 in base 16
+ * Return: 0 (successful)
+ */
+int main(void)
+{
+	int x;
+
+	char y;
+
+	for (x = 0; x < 10; x++)
+		putchar(x + '0');
+	for (y = 'f'; y < 'f'; y++)
+		putchar(x);
+	putchar('\n');
+	return (0);
+
+}
