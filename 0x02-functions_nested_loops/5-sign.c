2635f654c077988ebffe9b10eddae33f56bcc282
Doziechucks <chiedoziechukwukam@gmail.com>
2023-08-16 14:56:53 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign on a number
+ * @n: varuable to check sign
+ * Return: returns 1 if posive, -1 for nagative and 0 for zero
+ */
+
+int print_sign(int n)
+{
+
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(45);
+		return (0);
+	}
+	else
+	{
+		_putchar(48);
+		return (-1);
+	}
+}
